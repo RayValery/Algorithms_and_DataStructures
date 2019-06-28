f402d8cba563676d85a24f21fd859df60a508eb7
Valerie Salivon <ValerieSalivon@gmail.com>
2019-06-28 12:53:31 +0300

    Add files via upload
    Description of the type of data stored in the bidirectional list

---
+++ Data.java
@@ -0,0 +1,45 @@
+public class Data {
+
+    private String name;
+    private int course;
+    private Boolean army;
+
+    public Data()
+    {
+        name = "";
+        course = 0;
+        army = false;
+    }
+
+    public Data(String name, int course, Boolean army)
+    {
+        this.name = name;
+        this.course = course;
+        this.army = army;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCourse() {
+        return course;
+    }
+
+    public void setCourse(int course) {
+        this.course = course;
+    }
+
+    public Boolean getArmy() {
+        return army;
+    }
+
+    public void setArmy(Boolean army) {
+        this.army = army;
+    }
+}
+
